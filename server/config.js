90d57ac583c36c89e9482bd62dc825f1a3efcc73
donkats <60600316+donkats@users.noreply.github.com>
2020-04-20 18:20:43 +0200

    added database heroku credentials

--- server/config.js
+++ server/config.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 
 const { Pool } = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = true;
 
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: isProduction ? `postgres://mlqwcmydjqkpeb:caa92e81b72905e4cdb3a9731c3854026cbfac1c35e2f107725b47ad21c78672@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d3ettgngq3bba0`
+  : connectionString,
   ssl: isProduction,
 })
 
