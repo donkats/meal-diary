26a7a590f7f75ef23c9c5b1ef7d662050460970f
Sara Aerssens <s.c.aerssens@gmail.com>
2020-04-21 12:19:55 +0200

    updated popup

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -4,13 +4,27 @@ function hidePopup() {
   document.querySelector('.popup').classList.add('hidden');
 }
 
+function searchClick() {
+  const content = document.querySelector('.searchbar').value;
+  console.log('search bar content:', content);
+  if (content.trim() != '') {
+    // make fetch request here
+  }
+}
+
 function Popup() {
   return (
-    <div className="popup hidden">
+    <div className="popup">
       <div className="popup-inner">
         <span className="close-popup" onClick={() => hidePopup()}>&times;</span>
         <div className="popup-content">
-          I AM A POP UP!!!
+          <h3>Search for product:</h3>
+          <div className="search-container">
+            <input type="search" className="searchbar" placeholder="search..." />
+            <input type="image" className="search-icon" 
+              src="https://img.icons8.com/ios-filled/24/000000/search.png" alt="search icon"
+              onClick={() => searchClick()} />
+          </div>
         </div>
       </div>
     </div>
