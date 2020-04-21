ff8593f8e0a45c6a2c311c4ffb078eef225747d3
Milan Krstanovic <milan.krstanovic@gmail.com>
2020-04-21 22:19:04 +0200

    db functions working

---
+++ server/functions.js
@@ -0,0 +1,49 @@
+const { pool } = require('./config');
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const addUser = (request, response) => {
+  const { username, password, email, height, daily_goal, name, diet  } = request.body;
+  console.log(username, password);
+  // const info = [ 'newuser', 'newpass'];
+  pool.query('INSERT INTO users (username, password, email, height, daily_goal, name, diet) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+  [username, password, email, height, daily_goal, name, diet], error => {
+    if (error) {
+      throw error
+    }
+    response.status(201).json({ status: 'success', message: 'User added.' })
+  })
+};
+
+const addMeals = (request, response) => {
+  const { foodName, kcal, amount, userId, meal, grams, kcalIntake, date } = request.body;
+
+  pool.query('INSERT INTO ingredients (food_name, kcal, amount) VALUES ($1, $2, $3)', [foodName, kcal, amount], error => {
+    // add if not exist option
+    if (error) {
+      throw error
+    }
+    // response.status(201).json({ status: 'success', message: 'Ingredient added'})
+  });
+  pool.query('INSERT INTO meals (meal, grams, kcal_intake, date, users_id, ingredients_id) VALUES ($1, $2, $3, $4, $5, (select id from ingredients where food_name = $6))', 
+  [meal, grams, kcalIntake, date, userId, foodName], error => {
+    // add if not exist option
+    if (error) {
+      throw error
+    }
+    response.status(201).json({ status: 'success', message: 'Ingredient and meal added'})
+  });
+};
+
+module.exports = {
+  getUsers,
+  addUser,
+  addMeals
+}
--- server/index.js
+++ server/index.js
@@ -3,41 +3,23 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 
-const { pool } = require('./config');
+const db = require ('./functions');
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors());
 
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const addUser = (request, response) => {
-  const { username, password } = request.body
-
-  pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password], error => {
-    if (error) {
-      throw error
-    }
-    response.status(201).json({ status: 'success', message: 'User added.' })
-  })
-}
-
 app
   .route('/users')
-  // GET endpoint
-  .get(getUsers)
-  // POST endpoint
-  .post(addUser)
+  .get(db.getUsers)
+  .post(db.addUser)
+
+app
+  .route('/meals')
+  .post(db.addMeals);
 
 // Start server
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server listening`)
-})
+});
