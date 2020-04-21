bbbe0294fc3557fc097432862d18c898b2d436bd
Sara Aerssens <s.c.aerssens@gmail.com>
2020-04-21 11:40:27 +0200

    added popup

--- client/src/components/FoodOverview.js
+++ client/src/components/FoodOverview.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import Popup from './Popup';
+
+function showPopup() {
+  document.querySelector('.popup').classList.remove('hidden');
+}
 
 function FoodOverview() {
   return (
     <div className="overview-box">
       <h3>Breakfast</h3>
-      <div className="breakfast"></div>
-      <h3>Lunch</h3>
-      <div className="lunch"></div>
-      <h3>Dinner</h3>
-      <div className="dinner"></div>
-      <div className="add-btn-box">
-        <button className="add-btn">add food</button>
+      <div className="breakfast">
+        <button className="add-btn" onClick={() => showPopup()}>+ add breakfast</button>
       </div>
+      <h3>Lunch</h3>
+      <div className="lunch">
+        <button className="add-btn" onClick={() => showPopup()}> + add lunch</button>
+      </div>
+      <h3>Dinner</h3>
+      <div className="dinner">
+      <button className="add-btn" onClick={() => showPopup()}>+ add dinner</button>
+      </div>
+      <Popup />
     </div>
   );
 }
--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+function hidePopup() {
+  document.querySelector('.popup').classList.add('hidden');
+}
+
 function Popup() {
   return (
-    
+    <div className="popup hidden">
+      <div className="popup-inner">
+        <span className="close-popup" onClick={() => hidePopup()}>&times;</span>
+        <div className="popup-content">
+          I AM A POP UP!!!
+        </div>
+      </div>
+    </div>
   );
 }
 
