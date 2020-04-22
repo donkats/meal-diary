2f1d9db211bc956917f2d4b46cf99ed8306b7f54
Milan Krstanovic <milan.krstanovic@gmail.com>
2020-04-23 00:16:55 +0200

    added api-functions, parse data, comments

---
+++ server/api-functions.js
@@ -0,0 +1,119 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const searchUrl = 'https://trackapi.nutritionix.com/v2/search/instant';
+const appId = process.env.APP_ID;
+const appKey = process.env.APP_KEY;
+
+// helper function parsing data for general search
+function parseData(data) {
+  const common = data.common.map((food) => {
+    return {
+      foodName: food['food_name'],
+      servingUnit: food['serving_unit'],
+      servingQty: food['serving_qty'],
+      photo: food.photo,
+    }
+  });
+
+  const branded = data.branded.map((food) => {
+    return {
+      nixItemId: food['nix_item_id'],
+      foodName: food['food_name'],
+      brandName: food['brand_name'],
+      servingUnit: food['serving_unit'],
+      servingQty: food['serving_qty'],
+      calories: food['nf_calories'],
+      photo: food.photo,
+    }
+  });
+
+  return { common, branded };
+}
+//helper function parsing one common product data
+function parseCommonItem(data) {
+  const product = data.foods[0];
+    return {
+      foodName: product['food_name'],
+      servingUnit: product['serving_unit'],
+      servingQty: product['serving_qty'],
+      gramsUnit: product['serving_weight_grams'],
+      kcal: product['nf_calories'],
+      photo: product.photo['thumb']
+    }
+}
+//helper function parsing one branded product data
+function parseBrandedItem(data) {
+  const product = data.foods[0];
+    return {
+      foodName: product['food_name'],
+      servingUnit: product['serving_unit'],
+      servingQty: product['serving_qty'],
+      gramsUnit: product['serving_weight_grams'],
+      kcal: product['nf_calories'],
+      photo: product.photo['thumb'],
+      nixItemId: product['nix_item_id'],
+      brandName: product['brand_name']
+    }
+}
+// products search - returns list of products
+function fetchSearch(req, res) {
+  const query = req.params.query;
+  const queryUrl = searchUrl + '?query=' + query;
+
+  const fetchObj = {
+    method: 'GET',
+    headers: {
+      'x-app-id': appId,
+      'x-app-key': appKey
+    }
+  };
+  
+  fetch(queryUrl, fetchObj)
+    .then((data) => data.json())
+    .then((data) => res.json(parseData(data)));
+}
+// fetch for one product
+function fetchItem(req, res) {
+  const type = req.params.type;
+  const query = req.params.name;
+  console.log('query:', query)
+
+  const commonObj = {
+    method: 'POST',
+    headers: {
+      'x-app-id': appId,
+      'x-app-key': appKey,
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  };
+
+  const brandedObj = {
+    method: 'GET',
+    headers: {
+      'x-app-id': appId,
+      'x-app-key': appKey
+    }
+  }
+  console.log(commonObj.body);
+  const commonUrl = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
+  const brandedUrl = 'https://trackapi.nutritionix.com/v2/search/item';
+
+  if (type === 'common') {
+    fetch(commonUrl, commonObj)
+      .then((data) => data.json())
+      .then((data) => res.json(parseCommonItem(data)));
+  } else {
+    const idUrl = brandedUrl + '?nix_item_id=' + query;
+    console.log(idUrl);
+    fetch(idUrl, brandedObj)
+      .then((data) => data.json())
+      .then((data) => res.json(parseBrandedItem(data)));
+  }
+}
+
+module.exports = {
+  fetchSearch,
+  fetchItem
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
-const fetch = require('node-fetch');
 
-require('dotenv').config();
-
-const db = require ('./functions');
+const db = require ('./db-functions');
+const api = require('./api-functions')
 const app = express();
 
 app.use(bodyParser.json())
@@ -16,120 +14,36 @@ app
   .route('/users')
   .get(db.getUsers)
   .post(db.addUser)
-
+// database - post - posts in ingredients, then in meals
+// get - all meals per date per user
 app
   .route('/meals')
   .post(db.addMeals)
   .get(db.getAllMeals)
-
+// database - get breakfast rows per date per user
 app
   .route('/breakfast')
   .get(db.getBreakfast)
-
+// database - get lunch rows per date per user
 app
   .route('/lunch')
   .get(db.getLunch)
-
+// database - get dinner rows per date per user
 app
   .route('/dinner')
   .get(db.getDinner)
-
+// database - get amount of calories per date per user
 app
   .route('/calories')
   .get(db.getDailyCalories)
-
-const searchUrl = 'https://trackapi.nutritionix.com/v2/search/instant';
-const appId = process.env.APP_ID;
-const appKey = process.env.APP_KEY;
-
-function parseData(data) {
-  const common = data.common.map((food) => {
-    return {
-      foodName: food['food_name'],
-      servingUnit: food['serving_unit'],
-      servingQty: food['serving_qty'],
-      photo: food.photo,
-    }
-  });
-
-  const branded = data.branded.map((food) => {
-    return {
-      nixItemId: food['nix_item_id'],
-      foodName: food['food_name'],
-      brandName: food['brand_name'],
-      servingUnit: food['serving_unit'],
-      servingQty: food['serving_qty'],
-      calories: food['nf_calories'],
-      photo: food.photo,
-    }
-  });
-
-  return { common, branded };
-}
-
-function fetchSearch(req, res) {
-  const query = req.params.query;
-  const queryUrl = searchUrl + '?query=' + query;
-
-  const fetchObj = {
-    method: 'GET',
-    headers: {
-      'x-app-id': appId,
-      'x-app-key': appKey
-    }
-  };
-  
-  fetch(queryUrl, fetchObj)
-    .then((data) => data.json())
-    .then((data) => res.json(parseData(data)));
-}
-
+// api - get list of products
 app
   .route('/search/:query')
-  .get(fetchSearch);
-
-function fetchItem(req, res) {
-  const type = req.params.type;
-  const query = req.params.name;
-  console.log('query:', query)
-
-  const commonObj = {
-    method: 'POST',
-    headers: {
-      'x-app-id': appId,
-      'x-app-key': appKey,
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({ query })
-  };
-
-  const brandedObj = {
-    method: 'GET',
-    headers: {
-      'x-app-id': appId,
-      'x-app-key': appKey
-    }
-  }
-
-  const commonUrl = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
-  const brandedUrl = 'https://trackapi.nutritionix.com/v2/search/item';
-
-  if (type === 'common') {
-    fetch(commonUrl, commonObj)
-      .then((data) => data.json())
-      .then((data) => res.json(data));
-  } else {
-    const idUrl = brandedUrl + '?nix_item_id=' + query;
-    console.log(idUrl);
-    fetch(idUrl, brandedObj)
-      .then((data) => data.json())
-      .then((data) => res.json(data));
-  }
-}
-
+  .get(api.fetchSearch);
+// api - get details for one product
 app
   .route('/:type/:name')
-  .get(fetchItem);
+  .get(api.fetchItem);
 
 // Start server
 app.listen(process.env.PORT || 3001, () => {
