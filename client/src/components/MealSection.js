d58f2b9aedddb06f5e44c22a6cd1ab58ea6dc296
Sara Aerssens <s.c.aerssens@gmail.com>
2020-04-23 23:09:50 +0200

    added automatic refresh

--- client/src/components/MealSection.js
+++ client/src/components/MealSection.js
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import Popup from './Popup';
 
-function showPopup(meal) {
-  document.getElementById(meal).classList.remove('hidden');
-}
-
 function MealSection(props) {
   const { meal, userId, date } = props;
   const [meals, updateMeals] = useState([]);
 
   const totalKcal = meals.length > 0 ? meals.reduce((val, item) => val + item.kcal_intake, 0) : 0;
 
-  useEffect(() => {
+  const fetchMeals = () => {
     fetch(`/meals/${userId}/${meal}/${date}`)
-      .then((data) => data.json())
-      .then((data) => updateMeals(data));
-  }, []);
+        .then((data) => data.json())
+        .then((data) => updateMeals(data));
+  }
+
+  useEffect(() => fetchMeals(), []);
+
+  const showPopup = (meal) => {
+    document.getElementById(meal).classList.remove('hidden');
+  }
+  
+  const hidePopup = (meal) => {
+    document.getElementById(meal).classList.add('hidden');
+    fetchMeals();
+  }
 
   return (
     <div className="meal">
@@ -29,7 +36,7 @@ function MealSection(props) {
       ))}
       <button className="add-btn" onClick={() => showPopup(meal)}>+ add breakfast</button>
       <div className="total-kcal">Total: {totalKcal} kcal</div>
-      <Popup meal={meal}/>
+      <Popup meal={meal} hidePopup={hidePopup} />
     </div>
   )
 }
--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import ResultList from './ResultList';
 
-function hidePopup(meal) {
-  document.getElementById(meal).classList.add('hidden');
-}
-
 function fetchData(updateResults, meal) {
   const input = document.getElementById(`search${meal}`);
   const query = input.value.trim();
@@ -18,23 +14,24 @@ function fetchData(updateResults, meal) {
 }
 
 function Popup(props) {
+  const { meal, hidePopup, closePopup } = props;
   const [results, updateResults] = useState({});
 
   return (
-    <div className="popup hidden" id={props.meal}>
+    <div className="popup hidden" id={meal}>
       <div className="popup-inner">
-        <span className="close-popup" onClick={() => hidePopup(props.meal)}>&times;</span>
+        <span className="close-popup" onClick={() => hidePopup(meal, closePopup)}>&times;</span>
         <div className="popup-content">
           <div className="search-container">
-            <input type="search" id={`search${props.meal}`} className="searchbar" placeholder="search..." />
+            <input type="search" id={`search${meal}`} className="searchbar" placeholder="search..." />
             <input type="image" className="search-icon" 
               src="https://img.icons8.com/ios-filled/24/000000/search.png" alt="search icon"
               onClick={() => fetchData(updateResults, props.meal)} />
           </div>
           {Object.entries(results).length ? 
             <div className="results">
-              <ResultList list={results.common} meal={props.meal} type="common" />
-              <ResultList list={results.branded} meal={props.meal} type="branded" />
+              <ResultList list={results.common} meal={meal} type="common" />
+              <ResultList list={results.branded} meal={meal} type="branded" />
             </div>
             : ''}
         </div>
