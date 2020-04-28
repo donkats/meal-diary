c655a2d85200432c9ca1f28b881b09c7049b718a
donkats <60600316+donkats@users.noreply.github.com>
2020-04-28 20:12:59 +0200

    test

--- server/index.js
+++ server/index.js
@@ -11,6 +11,17 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app
   .route('/newusers')
   .post(db.addUser);
